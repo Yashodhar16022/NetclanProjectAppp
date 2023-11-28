70d7ca24f835a1e648ad57f8597b814f1dbec4a6
yashodhar.p.d <yashd126@>
2023-11-28 11:14:28 +0530

    THIS MY NEW ONE PROJECT

---
+++ app/src/main/java/com/example/netclanapp/BuisnessFragment.java
@@ -0,0 +1,28 @@
+package com.example.netclanapp;
+
+
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+
+
+public class BuisnessFragment extends Fragment {
+
+    public BuisnessFragment() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_buisness, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/netclanapp/ExploreScreen.java
@@ -0,0 +1,70 @@
+package com.example.netclanapp;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class ExploreScreen extends AppCompatActivity {
+    ImageView refine, menu;
+    TabLayout tab;
+    ViewPager2 viewPager;
+    ViewPagerAdapter ViewPagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_explore_screen);
+        tab = findViewById(R.id.tab);
+        viewPager = findViewById(R.id.viewPager);
+        ViewPagerAdapter = new ViewPagerAdapter(this);
+        viewPager.setAdapter(ViewPagerAdapter);
+        refine = findViewById(R.id.refine);
+        menu = findViewById(R.id.imageView);
+
+        tab.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                tab.getTabAt(position).select();
+            }
+        });
+        refine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ProfileScreen.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
